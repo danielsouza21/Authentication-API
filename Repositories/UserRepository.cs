5dcafda5a7b164bc1a20ab955091bffa0d377fce
danielsouza21 <danielloko1999@hotmail.com>
2021-02-16 22:22:17 -0300

    first commit

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Authenticator_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authenticator_API.Repositories
+{
+    public class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+
+            users.Add(new User { Id = 1, Username = "batman", Password = "batman", Role = "manager" });
+            users.Add(new User { Id = 2, Username = "robin", Password = "robin", Role = "employee" });
+            
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == x.Password).FirstOrDefault();
+        }
+    }
+}
